a95a7d67b0803375affe256cdf4880f29b6fb5ce
Carlos Virgílio Beltrão Lessa <carloslessa@hotmail.com>
2025-05-17 10:37:00 -0300

    Refatorar schemas Pydantic para melhor reutilização, validação e organização

--- app/config.py
+++ app/config.py
@@ -4,5 +4,7 @@ class Settings:
     SECRET_KEY = os.getenv("SECRET_KEY", "segredo-super-seguro")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 settings = Settings()
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin123"
